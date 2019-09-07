572d5666b4dea24ea4c023afce6eb5c2d4f7776b
StoneT2000 <stoneztao@gmail.com>
2019-09-06 22:13:39 -0700

    New Project and stylish cirlces

--- scripts/base.js
+++ scripts/base.js
@@ -2,10 +2,27 @@ $(document).ready(function(){
   window.onscroll = function(){
     if (window.pageYOffset > 135) {
       $("header").addClass('shadow');
+      $(".active").css("border-bottom", "4px solid #fefefe");
     }
     else {
       $("header").removeClass('shadow');
+      $(".active").removeAttr('style');
     }
   }
   window.onscroll();
+  
+  addCodeCircles("java", "Java");
+  addCodeCircles("tensorflow", "TensorFlow");
+  addCodeCircles("p5js", "p5.js");
+  addCodeCircles("js", "JS");
+  addCodeCircles("html", "HTML");
+  addCodeCircles("css", "CSS");
+  addCodeCircles("htmlcss", "HTML & CSS");
+  
+  
+  
 });
+var colors = {css:"#563d7c" , java:"#b07219", html:"#e34c26", js:"#f1e05a", p5js:"#ed225d",tensorflow:"#ff6f00",htmlcss:"#85425F"};
+function addCodeCircles(name, displayName) {
+  $(".project." + name +" > .code-compositions").append("<span><div class='code-circle' style='background-color:" + colors[name] +"'></div><span class='code-circle-def'>" + displayName + "</span></span>");
+}
