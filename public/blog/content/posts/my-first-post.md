509cf37073d1dc44e1deeb7d5f795d612c57d001
stone_tao <stonezt2019@gmail.com>
2022-09-02 00:27:50 -0700

    hugo blog

---
+++ public/blog/content/posts/my-first-post.md
@@ -0,0 +1,6 @@
+---
+title: "My First Post"
+date: 2022-09-02T00:22:33-07:00
+draft: true
+---
+
--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/projects" component={Projects} />
         {/* <Route exact path="/about" component={About} /> */}
-        <Route exact path="/blog" component={Blog} />
+        {/* <Route exact path="/blog" component={Blog} /> */}
         <Route
           exact
           strict
