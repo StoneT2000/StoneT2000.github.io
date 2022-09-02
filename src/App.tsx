625921932f5cfaf6096fadc6d2dd13bd71595707
stone_tao <stonezt2019@gmail.com>
2022-09-02 02:30:47 -0700

    moved blog post

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Switch, Route, useLocation, Redirect } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
 import './styles/base.css';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
-import Blog from './pages/Blog';
-import BlogPostPage from './pages/BlogPostPage';
+// import Blog from './pages/Blog';
+// import BlogPostPage from './pages/BlogPostPage';
 import ReactGA from 'react-ga';
 function App() {
   const location = useLocation();
@@ -24,8 +24,12 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/projects" component={Projects} />
         {/* <Route exact path="/about" component={About} /> */}
-        <Route exact path="/blog" component={Blog} />
-        <Route
+        {/* <Route exact path="/blog" component={Blog} /> */}
+        <Route path="/blog" component={() => { 
+            window.location.href = 'https://blog.stoneztao.com'; 
+            return null;
+        }} />
+        {/* <Route
           exact
           strict
           path="/blog/posts/:postpath"
@@ -42,7 +46,7 @@ function App() {
           strict
           path="/blog/posts/:postpath/"
           component={BlogPostPage}
-        />
+        /> */}
       </Switch>
     </div>
   );
