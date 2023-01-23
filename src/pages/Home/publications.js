1e1f4c0bc618eb5ba2179747b487068aedf69d04
stone_tao <stonezt2019@gmail.com>
2023-01-22 20:34:22 -0800

    update

--- src/pages/Home/publications.js
+++ src/pages/Home/publications.js
@@ -4,7 +4,7 @@ export const data = [
       'Abstract-to-Executable Trajectory Translation for One-Shot Task Generalization',
     authors:
       'Stone Tao, Xiaochen Li, Tongzhou Mu, Zhiao Huang, Yuzhe Qin, and Hao Su.',
-    venue: 'In Submission to ICLR 2023; Accepted at NeurIPS 2022 Deep RL Workshop',
+    venue: 'In Submission to ICML 2023; Accepted at NeurIPS 2022 Deep RL Workshop',
     year: '2023',
     link: 'https://arxiv.org/abs/2210.07658',
     projectpage: 'https://trajectorytranslation.github.io/',
@@ -28,7 +28,7 @@ export const data = [
       'ManiSkill2: A Unified Benchmark for Generalizable Manipulation Skills',
     authors:
       'Jiayuan Gu, Fanbo Xiang, Zhan Ling, Xinyue Wei, Xiqiang Liu, Xuanlin Li, Rui Chen, Stone Tao, Tongzhou Mu, Pengwei Xie, Yunchao Yao, Yihe Tang, Xiaodi Yuan, Zhiao Huang, Hao Su.',
-    venue: 'In Submission to ICLR 2023',
+    venue: 'ICLR 2023',
     year: '2023',
     link: 'TBA',
     projectpage: 'https://sapien.ucsd.edu/challenges/maniskill/2022/',
